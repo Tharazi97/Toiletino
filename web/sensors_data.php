580c244558627c5c40c905b7a5e9d4defa214949
Szymon Kwiatkowski <Kwiatkowski.Szymon97@gmail.com>
2020-06-24 21:17:03 +0200

    Add normalization of data

--- web/sensors_data.php
+++ web/sensors_data.php
@@ -21,13 +21,40 @@
 
     switch ($graphType) {
         case 0: // pollution
-            $query = 'SELECT sensTime AS time, sensor0, sensor1, sensor2, sensor3, brightness FROM'. $dbName.'.readings WHERE sensTime BETWEEN TIMESTAMP('.$_GET["dateFrom"].') AND TIMESTAMP('.$_GET["dateTo"].')';
+            $query = 'SELECT sensTime AS time, 
+                            (100 - LOG10(376/sensor0 - 0.367) * 100) AS sensor0, 
+                            (100 - LOG10(771.5/sensor1 - 0.753) * 100) AS sensor1, 
+                            (100 - LOG10(1102.9/sensor2 - 1.077) * 100) AS sensor2, 
+                            (100 - LOG10(364.84/sensor3 - 0.356) * 100) AS sensor3, 
+                            brightness 
+                        FROM db697406.readings 
+                        WHERE sensTime BETWEEN TIMESTAMP('.$_GET["dateFrom"].') AND TIMESTAMP('.$_GET["dateTo"].')';
             break;
         case 1: // temperature
-            $query = 'SELECT sensTime AS time, temperature FROM '. $dbName.'.readings WHERE sensTime BETWEEN TIMESTAMP('.$_GET["dateFrom"].') AND TIMESTAMP('.$_GET["dateTo"].')';
+            $query = 'SELECT sensTime AS time, 
+                            temperature, 
+                            brightness 
+                        FROM db697406.readings 
+                        WHERE sensTime BETWEEN TIMESTAMP('.$_GET["dateFrom"].') AND TIMESTAMP('.$_GET["dateTo"].')';
             break;
         case 2: // humidity
-            $query = 'SELECT sensTime AS time, humidity FROM '. $dbName.'.readings WHERE sensTime BETWEEN TIMESTAMP('.$_GET["dateFrom"].') AND TIMESTAMP('.$_GET["dateTo"].')';
+            $query = 'SELECT sensTime AS time, 
+                            humidity, 
+                            brightness 
+                        FROM db697406.readings 
+                        WHERE sensTime BETWEEN TIMESTAMP('.$_GET["dateFrom"].') AND TIMESTAMP('.$_GET["dateTo"].')';
+            break;
+        case 3: // all
+            $query = 'SELECT sensTime AS time, 
+                            (100 - LOG10(376/sensor0 - 0.367) * 100) AS sensor0, 
+                            (100 - LOG10(771.5/sensor1 - 0.753) * 100) AS sensor1, 
+                            (100 - LOG10(1102.9/sensor2 - 1.077) * 100) AS sensor2, 
+                            (100 - LOG10(364.84/sensor3 - 0.356) * 100) AS sensor3, 
+                            brightness, 
+                            temperature, 
+                            humidity 
+                        FROM db697406.readings 
+                        WHERE sensTime BETWEEN TIMESTAMP('.$_GET["dateFrom"].') AND TIMESTAMP('.$_GET["dateTo"].')';
             break;
         default:
             die("Wrong graphType");
